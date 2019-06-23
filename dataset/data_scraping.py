db2dcd910c35c40e5f633805cb4c2cbb19f2e68d
Hrithik Auchar <49407332+hvauchar@users.noreply.github.com>
2019-06-23 10:34:23 +0530

    Update data_scraping.py

--- dataset/data_scraping.py
+++ dataset/data_scraping.py
@@ -5,6 +5,19 @@ driver.get('https://physionet.org/cgi-bin/atm/ATM?database=ctu-uhb-ctgdb&tool=sa
 import requests 
 main_url = "https://physionet.org/atm/ctu-uhb-ctgdb/"
 
+
+for i in range(1001,2001):
+    select = Select(driver.find_element_by_name('rbase'))
+    select.select_by_visible_text(str(i))
+    driver.find_element_by_xpath(".//input[@type='radio' and @value='e']").click()
+    url= main_url+ str(i)+"/0/e/rdsamp/csv/pd/samples.csv"
+    print(url)
+    print(i)
+    r = requests.get(url) 
+    with open(str(i)+".csv",'wb') as f: 
+        f.write(r.content)
+
+
 for i in range(2001,2047):
     select = Select(driver.find_element_by_name('rbase'))
     select.select_by_visible_text(str(i))
